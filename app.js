a16bab929f1fd58362c50758072104b8970a7ee3
Enoch Ko <enoch232@gmail.com>
2016-07-08 23:19:11 -0700

    use demo to test

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-var express = require("express");
-var app = express();
+var request = require("request"),
+  cheerio = require("cheerio"),
+  url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 02888;
+  
+request(url, function (error, response, body) {
+  if (!error) {
+    var $ = cheerio.load(body),
+      temperature = $("[data-variable='temperature'] .wx-value").html();
+      
+    console.log("It’s " + temperature + " degrees Fahrenheit.");
+  } else {
+    console.log("We’ve encountered an error: " + error);
+  }
+});
