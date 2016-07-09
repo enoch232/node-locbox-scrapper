6b11223366594a75d869bda638f410baa0c02271
Enoch Ko <enoch232@gmail.com>
2016-07-09 10:56:53 -0700

    add prompt

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 var request = require("request");
 var cheerio = require("cheerio");
+var prompt = require("prompt");
+
 var url = "https://www.groupon.com/browse/phoenix?lat=33.4483&lng=-112.074&address=Phoenix&query=yoga&locale=en_US";
 request(url, function (error, response, body) {
   if (!error) {
     var $ = cheerio.load(body);
+    //get all the business names
     var business_names = $(".cui-merchant-name.cui-truncate").toArray();
-    var count = $(".cui-details-container").toArray().length;
+    //obtain length of the business_name array
+    var count = business_names.length;
     console.log("There are " + count);
+    //Go through the array and display each names. The name strings are located under its elements' first child
     for(var i =0; i< count; i++){
     	if (business_names[i] && business_names[i].children[0]){
     		console.log(business_names[i].children[0].data);
     	}
     }
+    //get all divs with class cui-content, which are parents of element with the website's url string. 
     var business_websites = $(".cui-content").toArray();
+    //Go through the array and display each href. The href strings are located under its elements' attribs of second child.
     for(var i = 0; i< count; i++){
     	if (business_websites[i].children[1] && business_websites[i]){
     		console.log(business_websites[i].children[1].attribs.href);
     	}
     }
-
-
-
   } else {
     console.log("We’ve encountered an error: " + error);
   }
