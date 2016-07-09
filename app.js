e032d2088fdb1855607edd4e3cd9c9f54b034b6f
Enoch Ko <enoch232@gmail.com>
2016-07-09 10:36:05 -0700

    change variable names, and make so that it looks through the whole page

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-var request = require("request"),
-  cheerio = require("cheerio"),
-  url = "https://www.groupon.com/browse/phoenix?lat=33.4483&lng=-112.074&address=Phoenix&query=massage&locale=en_US"
-  
+var request = require("request");
+var cheerio = require("cheerio");
+var url = "https://www.groupon.com/browse/phoenix?lat=33.4483&lng=-112.074&address=Phoenix&query=yoga&locale=en_US";
 request(url, function (error, response, body) {
   if (!error) {
-    var $ = cheerio.load(body),
-      temperature = $(".cui-merchant-name.cui-truncate").toArray();
-    for(var i =0; i< 10; i++){
-    	console.log(temperature[i].children[0].data);
+    var $ = cheerio.load(body);
+    var business_names = $(".cui-merchant-name.cui-truncate").toArray();
+    var count = $(".cui-details-container").toArray().length;
+    console.log("There are " + count);
+    for(var i =0; i< count; i++){
+    	if (business_names[i] && business_names[i].children[0]){
+    		console.log(business_names[i].children[0].data);
+    	}
+    }
+    var business_websites = $(".cui-content").toArray();
+    for(var i = 0; i< count; i++){
+    	if (business_websites[i].children[1] && business_websites[i]){
+    		console.log(business_websites[i].children[1].attribs.href);
+    	}
+    }
 
-    }
-    var cool = $(".cui-content").toArray();
-    for(var i = 0; i< 10; i++){
-    	console.log(cool[i].children[1].attribs.href);
-    }
+
+
   } else {
     console.log("We’ve encountered an error: " + error);
   }
