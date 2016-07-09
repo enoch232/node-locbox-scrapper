57febf3039e034c1ef17654ba620564d0cf2b848
Enoch Ko <enoch232@gmail.com>
2016-07-09 10:12:14 -0700

    able to scrape the names, and the websites of each company on the page

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ request(url, function (error, response, body) {
   if (!error) {
     var $ = cheerio.load(body),
       temperature = $(".cui-merchant-name.cui-truncate").toArray();
-      
     for(var i =0; i< 10; i++){
-    	console.log(temperature[i]);
+    	console.log(temperature[i].children[0].data);
+
+    }
+    var cool = $(".cui-content").toArray();
+    for(var i = 0; i< 10; i++){
+    	console.log(cool[i].children[1].attribs.href);
     }
   } else {
     console.log("We’ve encountered an error: " + error);
