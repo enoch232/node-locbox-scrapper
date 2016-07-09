2372262464b6d84c8b8258a10499dade6a098b29
Enoch Ko <enoch232@gmail.com>
2016-07-08 23:50:43 -0700

    get only the groupon's names for first 10

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var request = require("request"),
   cheerio = require("cheerio"),
-  url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 02888;
+  url = "https://www.groupon.com/browse/phoenix?lat=33.4483&lng=-112.074&address=Phoenix&query=massage&locale=en_US"
   
 request(url, function (error, response, body) {
   if (!error) {
     var $ = cheerio.load(body),
-      temperature = $("[data-variable='temperature'] .wx-value").html();
+      temperature = $(".cui-merchant-name.cui-truncate").toArray();
       
-    console.log("It’s " + temperature + " degrees Fahrenheit.");
+    for(var i =0; i< 10; i++){
+    	console.log(temperature[i]);
+    }
   } else {
     console.log("We’ve encountered an error: " + error);
   }
